8973fc002c2e40be71a8a2e30f5a4f3ba168800c
K. "pestophagous" Heller <pestophagous@gmail.com>
2015-10-11 13:58:18 -0700

    even more quick-and-dirtiness to what has only ever been a quick-and-dirty 'my machine only' kind of build script

--- scripts/sbuild.sh
+++ scripts/sbuild.sh
@@ -78,3 +78,74 @@ fi
 
 LIBRARY_PATH=$INSTALL_ROOT/lib make -j4
 LIBRARY_PATH=$INSTALL_ROOT/lib make install
+
+
+# the '-B' is probably a bad idea. :) it forces every build command to run, regardless of 'no changes since last build'
+# LIBRARY_PATH=$INSTALL_ROOT/lib make -B TestDiveSiteDuplication \
+# 	    TestGitStorage \
+# 	    TestGpsCoords \
+# 	    TestParse \
+# 	    TestPlan \
+# 	    TestProfile \
+# 	    TestRenumber \
+# 	    TestUnitConversion
+
+
+#   "CMakeFiles/TestDiveSiteDuplication_automoc.dir/AutogenInfo.cmake"
+#   "CMakeFiles/TestGitStorage_automoc.dir/AutogenInfo.cmake"
+#   "CMakeFiles/TestGpsCoords_automoc.dir/AutogenInfo.cmake"
+#   "CMakeFiles/TestParse_automoc.dir/AutogenInfo.cmake"
+#   "CMakeFiles/TestPlan_automoc.dir/AutogenInfo.cmake"
+#   "CMakeFiles/TestProfile_automoc.dir/AutogenInfo.cmake"
+#   "CMakeFiles/TestRenumber_automoc.dir/AutogenInfo.cmake"
+#   "CMakeFiles/TestUnitConversion_automoc.dir/AutogenInfo.cmake"
+#   "CMakeFiles/TestDiveSiteDuplication.dir/DependInfo.cmake"
+#   "CMakeFiles/TestDiveSiteDuplication_automoc.dir/DependInfo.cmake"
+#   "CMakeFiles/TestGitStorage.dir/DependInfo.cmake"
+#   "CMakeFiles/TestGitStorage_automoc.dir/DependInfo.cmake"
+#   "CMakeFiles/TestGpsCoords.dir/DependInfo.cmake"
+#   "CMakeFiles/TestGpsCoords_automoc.dir/DependInfo.cmake"
+#   "CMakeFiles/TestParse.dir/DependInfo.cmake"
+#   "CMakeFiles/TestParse_automoc.dir/DependInfo.cmake"
+#   "CMakeFiles/TestPlan.dir/DependInfo.cmake"
+#   "CMakeFiles/TestPlan_automoc.dir/DependInfo.cmake"
+#   "CMakeFiles/TestProfile.dir/DependInfo.cmake"
+#   "CMakeFiles/TestProfile_automoc.dir/DependInfo.cmake"
+#   "CMakeFiles/TestRenumber.dir/DependInfo.cmake"
+#   "CMakeFiles/TestRenumber_automoc.dir/DependInfo.cmake"
+#   "CMakeFiles/TestUnitConversion.dir/DependInfo.cmake"
+#   "CMakeFiles/TestUnitConversion_automoc.dir/DependInfo.cmake"
+
+#   TestDiveSiteDuplication: CMakeFiles/TestDiveSiteDuplication.dir/rule
+# TestDiveSiteDuplication_automoc: CMakeFiles/TestDiveSiteDuplication_automoc.dir/rule
+# TestGitStorage: CMakeFiles/TestGitStorage.dir/rule
+# TestGitStorage_automoc: CMakeFiles/TestGitStorage_automoc.dir/rule
+# TestGpsCoords: CMakeFiles/TestGpsCoords.dir/rule
+# TestGpsCoords_automoc: CMakeFiles/TestGpsCoords_automoc.dir/rule
+# TestParse: CMakeFiles/TestParse.dir/rule
+# TestParse_automoc: CMakeFiles/TestParse_automoc.dir/rule
+# TestPlan: CMakeFiles/TestPlan.dir/rule
+# TestPlan_automoc: CMakeFiles/TestPlan_automoc.dir/rule
+# TestProfile: CMakeFiles/TestProfile.dir/rule
+# TestProfile_automoc: CMakeFiles/TestProfile_automoc.dir/rule
+# TestRenumber: CMakeFiles/TestRenumber.dir/rule
+# TestRenumber_automoc: CMakeFiles/TestRenumber_automoc.dir/rule
+# TestUnitConversion: CMakeFiles/TestUnitConversion.dir/rule
+# TestUnitConversion_automoc: CMakeFiles/TestUnitConversion_automoc.dir/rule
+
+# 	$(MAKE) -f CMakeFiles/TestDiveSiteDuplication.dir/build.make CMakeFiles/TestDiveSiteDuplication.dir/qrc_subsurface.cpp.o
+# 	$(MAKE) -f CMakeFiles/TestGitStorage.dir/build.make CMakeFiles/TestGitStorage.dir/qrc_subsurface.cpp.o
+# 	$(MAKE) -f CMakeFiles/TestGpsCoords.dir/build.make CMakeFiles/TestGpsCoords.dir/qrc_subsurface.cpp.o
+# 	$(MAKE) -f CMakeFiles/TestParse.dir/build.make CMakeFiles/TestParse.dir/qrc_subsurface.cpp.o
+# 	$(MAKE) -f CMakeFiles/TestPlan.dir/build.make CMakeFiles/TestPlan.dir/qrc_subsurface.cpp.o
+# 	$(MAKE) -f CMakeFiles/TestProfile.dir/build.make CMakeFiles/TestProfile.dir/qrc_subsurface.cpp.o
+# 	$(MAKE) -f CMakeFiles/TestRenumber.dir/build.make CMakeFiles/TestRenumber.dir/qrc_subsurface.cpp.o
+# 	$(MAKE) -f CMakeFiles/TestUnitConversion.dir/build.make CMakeFiles/TestUnitConversion.dir/qrc_subsurface.cpp.o
+# 	$(MAKE) -f CMakeFiles/TestDiveSiteDuplication.dir/build.make CMakeFiles/TestDiveSiteDuplication.dir/tests/testdivesiteduplication.cpp.o
+# 	$(MAKE) -f CMakeFiles/TestGitStorage.dir/build.make CMakeFiles/TestGitStorage.dir/tests/testgitstorage.cpp.o
+# 	$(MAKE) -f CMakeFiles/TestGpsCoords.dir/build.make CMakeFiles/TestGpsCoords.dir/tests/testgpscoords.cpp.o
+# 	$(MAKE) -f CMakeFiles/TestParse.dir/build.make CMakeFiles/TestParse.dir/tests/testparse.cpp.o
+# 	$(MAKE) -f CMakeFiles/TestPlan.dir/build.make CMakeFiles/TestPlan.dir/tests/testplan.cpp.o
+# 	$(MAKE) -f CMakeFiles/TestProfile.dir/build.make CMakeFiles/TestProfile.dir/tests/testprofile.cpp.o
+# 	$(MAKE) -f CMakeFiles/TestRenumber.dir/build.make CMakeFiles/TestRenumber.dir/tests/testrenumber.cpp.o
+# 	$(MAKE) -f CMakeFiles/TestUnitConversion.dir/build.make CMakeFiles/TestUnitConversion.dir/tests/testunitconversion.cpp.o
